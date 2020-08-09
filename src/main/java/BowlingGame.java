5161c9cecde0b8876a0c240a50014c86d49f7636
xiaomeiqi <805560812@qq.com>
2020-08-10 01:12:37 +0800

    when all 10 the sum is 300

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,55 +1,47 @@
-import java.util.Scanner;
+import java.util.Arrays;
+import java.util.List;
 
 public class BowlingGame {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int[][] scores = new int[11][4];
-
-        for (int i = 0; i < scores.length; i++) {
-            if (i < scores.length - 1) {
-                System.out.printf("请输入第%d轮，第1次打倒的个数：", i + 1);
-                scores[i][0] = sc.nextInt();
-                if (scores[i][0] < 10 || i == 9) {
-                    System.out.printf("请输入第%d轮，第2次打倒的个数：", i + 1);
-                    scores[i][1] = sc.nextInt();
-                }
-            } else if (scores[i - 1][0] + scores[i - 1][1] >= 10) {
-                System.out.printf("请输入第%d轮,第%d次打倒的个数：", i, 3);
-                scores[i][0] = scores[i][2] = sc.nextInt();
-            }
-        }
-
-        int sum = BowlingGame.sum(scores);
-        System.out.println(sum);
-
+    int[] bowlingScore = new int[22];
+    int ball=0;
+    int score;//分数
+    int count=0;
+    boolean firstThrow=true;
+    public void add(int grade){
+        bowlingScore[ball++] = grade;
+        isFirstTime(grade);
     }
-    public static int sum(int[][] scores){
-        if (scores == null){
-            throw new RuntimeException("input not valid");
-        }
 
-        for (int i = 0; i < scores.length-1; i++) {
-            if (scores[i][0] == 10 && i != 9) {
-                if (scores[i + 1][0] == 10 && i < 8){
-                    scores[i][2] = scores[i][0] + scores[i + 1][0]	+ scores[i + 2][0];
-                }else if (scores[i + 1][0] != 10 || i == 8){
-                    scores[i][2] = scores[i][0]  + scores[i + 1][0]  + scores[i + 1][1];
-                }
+    public void isFirstTime(int pins){
+        if(firstThrow){
+            if(pins==10){
+                count++;
             }else{
-                if (scores[i][0] + scores[i][1] == 10 && i < 9){
-                    scores[i][2] = scores[i][0] + scores[i][1] + scores[i + 1][0];
+                firstThrow=false;
+            }
+        }else{
+            firstThrow=true;
+            count++;
+        }
+    }
+
+    public int scoreForFrame(int frame){
+        int score=0;
+        int ball=0;
+        for(int count=0;count<frame;count++){
+            int firstThrow = bowlingScore[ball++];
+            if(firstThrow==10){
+                score= score+10+bowlingScore[ball]+bowlingScore [ball+1];
+            }else{
+                int secondThrow = bowlingScore[ball++];
+                int framScore = firstThrow+secondThrow;
+                if(framScore==10){
+                    score=framScore+bowlingScore[ball]+score;
                 }else{
-                    scores[i][2] = scores[i][0] + scores[i][1];
+                    score = framScore+score;
                 }
             }
         }
-
-        scores[0][3] = scores[0][2];
-        for(int i = 1; i < scores.length;i++ ){
-            scores[i][3]=scores[i][2] + scores[i - 1][3];
-        }
-
-        return scores[scores.length-1][scores[0].length-1];
+        return score;
     }
 }
-
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -5,33 +5,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class BowlingGameTest {
 
+
     @Test
     void should_return_300_when_all_hit(){
-        int[][] scores={{10,0,0,0},{10,0,0,0},{10,0,0,0},{10,0,0,0},{10,0,0,0},{10,0,0,0},{10,0,0,0},{10,0,0,0},{10,0,0,0},{10,10,0,0},{10,0,10,0}};
-        int result = BowlingGame.sum(scores);
-        int expected = 300;
-        assertEquals(result,expected);
+        int[] scores={10,10,10,10,10,10,10,10,10,10,10,10,10};
+        BowlingGame bowlingGame=new BowlingGame();
+        for (int i = 0; i < 13; i++) {
+            bowlingGame.add(scores[i]);
+        }
+        int result = bowlingGame.scoreForFrame(10);
+        assertEquals(300,result);
     }
 
-    @Test
-    void should_return_103_when_time_of_10_add_is_10(){
-        int[][] scores={{1,2,0,0},{3,4,0,0},{5,6,0,0},{7,8,0,0},{9,10,0,0},{1,2,0,0},{3,4,0,0},{5,6,0,0},{7,8,0,0},{9,1,0,0},{2,0,2,0}};
-        int result =BowlingGame.sum(scores);
-        int expected = 103;
-        assertEquals(result,expected);
-    }
 
-    @Test
-    void should_return_94_when_time_of_10_add_less_than_10(){
-        int[][] scores={{1,2,0,0},{3,4,0,0},{5,6,0,0},{7,8,0,0},{9,10,0,0},{1,2,0,0},{3,4,0,0},{5,6,0,0},{7,8,0,0},{1,2,0,0},{0,0,0,0}};
-        int result =BowlingGame.sum(scores);
-        int expected = 94;
-        assertEquals(result,expected);
-    }
-
-    @Test
-    void should_throw_exception_when_input_is_null(){
-        int[][] scores = null;
-        assertThrows(RuntimeException.class,()-> BowlingGame.sum(scores));
-    }
 }
