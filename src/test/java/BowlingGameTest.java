155560c12477756765d33b18bf4303897c420f34
xiaomeiqi <805560812@qq.com>
2020-08-10 01:15:27 +0800

    when no hit the sum is 0

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,20 +1,28 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-
-
 public class BowlingGameTest {
-
 
     @Test
     void should_return_300_when_all_hit(){
         int[] scores={10,10,10,10,10,10,10,10,10,10,10,10,10};
         BowlingGame bowlingGame=new BowlingGame();
-        for (int i = 0; i < 13; i++) {
+        for (int i = 0; i < scores.length; i++) {
             bowlingGame.add(scores[i]);
         }
         int result = bowlingGame.scoreForFrame(10);
         assertEquals(300,result);
+    }
+
+    @Test
+    void should_return_0_when_No_hit(){
+        int[] scores={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+        BowlingGame bowlingGame=new BowlingGame();
+        for (int i = 0; i < scores.length; i++) {
+            bowlingGame.add(scores[i]);
+        }
+        int result = bowlingGame.scoreForFrame(10);
+        assertEquals(0,result);
     }
 
 
